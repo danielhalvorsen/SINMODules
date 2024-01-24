0ca4b1003dfa24f5d0c29b47cb1764fd821f5429
Eva Chamorro Garrido <ega063@login-4.saga>
2024-01-24 12:03:06 +0100

    Add nan function

--- load_nc.py
+++ load_nc.py
@@ -9,3 +9,34 @@ def load_nc_file(filepath):
     filestruct = Dataset(filepath,'r')
     return filestruct
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def eva(bio):
+    bio[np.where(bio=9.9692100e+36)]=np.nan
+    return bio
