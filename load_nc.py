75cb1c8a8812adbb82b18c847a2a919db7916e83
danielhalvorsen <daniel.oe.halvorsen@gmail.com>
2024-01-24 12:00:47 +0100

    Added a temporary function to check merge-conflict

--- load_nc.py
+++ load_nc.py
@@ -8,4 +8,27 @@ import pandas as pd  # cian
 def load_nc_file(filepath):
     filestruct = Dataset(filepath,'r')
     return filestruct
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  def daniel_function_to_test_pull_request():
+      return 0
+
+
+
